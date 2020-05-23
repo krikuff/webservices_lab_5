be1a3c9382cf51429295226e36814e0e7dd83bf0
bmarid <mary.baj@yandex.ru>
2020-05-24 01:06:20 +0700

    commit

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,6 +1,7 @@
-print("Hello, world! from printf")
+# print("Hello, world! from printf")
 
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -13,3 +14,12 @@ def hello():
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
+
+
+@app.route("/data_to")
+def data_to():
+    some_pars = {'user': 'Ivan', 'color': 'red'}
+    some_str = 'Hello my dear frend!'
+    some_value = 10
+    return render_template('simple.html', some_str=some_str,
+                           some_value=some_value, some_pars=some_pars)

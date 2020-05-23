727ba2c7369ee3be13b85ee3e10f108a9c305687
bmarid <mary.baj@yandex.ru>
2020-05-24 00:04:34 +0700

    travis hello

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,2 +1,14 @@
 print("Hello, world!")
 
+
+from flask import Flask
+app=Flask(__name__)
+
+#dec to print
+@app.route("/")
+def hello():
+    return "<html><head></head><bodu>Hello World </body></html>"
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)

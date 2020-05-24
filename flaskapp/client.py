494ca71304cfd6e69be926f654fa4fe8cfbddeac
bmarid <mary.baj@yandex.ru>
2020-05-24 18:08:09 +0700

    add net

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,7 +1,7 @@
 import requests
-# r = requests.get('http://localhost:5000/')
-# print(r.status_code)
-# print(r.text)
+r = requests.get('http://localhost:5000/net')
+print(r.status_code)
+print(r.text)
 r = requests.get('http://localhost:5000/data_to')
 print(r.status_code)
 print(r.text)

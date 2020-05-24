04af21a65049a0bcc3a18f33a38a1dbb11f9d2ff
user <user@deb9amd64>
2020-05-24 16:23:30 +0700

    Fix code

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -6,22 +6,6 @@ if __name__ == "__main__":
 
 from flask import render_template
 @app.route("/data_to")
-def deta_to():
-    some_pars = {'user':'Ivan','color':'red'}
-    some_str = 'Hello my dear friends!'
-    some_value = 10
-    return render_template('simple.html', some_str = some_str,
-    some_value = some_value, some_pars = some_pars)
-
-from flask import render_template
-
-app = Flask(__name__)
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
-
-
-@app.route("/data_to")
 def data_to():
     some_pars = {'user': 'Ivan', 'color': 'red'}
     some_str = 'Hello my dear frend!'

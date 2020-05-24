bf8d0d9699f8a9e3c3fa737bf4c24a2e939d7ede
bmarid <mary.baj@yandex.ru>
2020-05-24 18:52:59 +0700

    add bootstrap

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,6 +1,8 @@
+from flask_bootstrap import Bootstrap
 from flask import Flask
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)

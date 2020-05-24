183b3f4f6d792f6589e5c3d36949b95e8d2c35a7
bmarid <mary.baj@yandex.ru>
2020-05-24 18:32:52 +0700

    add secret

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -27,6 +27,7 @@ app.config['RECAPTCHA_USE_SSL'] = False
 app.config['RECAPTCHA_PUBLIC_KEY'] = '6LewZvsUAAAAAP8ftBu1PhFkI5RTayByO83wbV0C'
 app.config['RECAPTCHA_PRIVATE_KEY'] = '6LewZvsUAAAAANbjfnDQ-mTr77JZDSqaJSEQ8lM_'
 app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
+app.config['SECRET_KEY'] = 'secret'
 
 
 # создаем форму для загрузки файла

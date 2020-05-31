2e6797e55a15f89331c36dbbea0ae597ecd692b5
bmarid <mary.baj@yandex.ru>
2020-05-31 18:41:47 +0700

    filter test

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -36,3 +36,20 @@ try:
     print(r.text)
 except:
     exit(1)
+
+
+# photo
+# img_data = None
+# создаем путь к файлу (для кроссплатформенности, например)
+# path = os.path.join('./static','image0008.png')
+# читаем файл и енкодируем его в строку base64
+with open(path, 'rb') as fh:
+    img_data = fh.read()
+    b64 = base64.b64encode(img_data)
+# создаем json словарь, который
+# отправляется на сервер в виде json строки
+# преобразование делает сама функция отправки запроса post
+# jsondata = {'imagebin':b64.decode('utf-8')}
+res = requests.post('http://localhost:5000/imgfilter')
+if res.ok:
+    print(res.json())

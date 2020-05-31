b0b5a3550404a93b115022b8924feed77907dd23
bmarid <mary.baj@yandex.ru>
2020-05-31 19:26:45 +0700

    filter test2, del dec

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -14,7 +14,7 @@ import base64
 
 img_data = None
 # создаем путь к файлу (для кроссплатформенности, например)
-path = os.path.join('./static','image0008.png')
+path = os.path.join('./static', 'image0008.png')
 # читаем файл и енкодируем его в строку base64
 with open(path, 'rb') as fh:
     img_data = fh.read()
@@ -38,18 +38,11 @@ except:
     exit(1)
 
 
-# photo
-# img_data = None
-# создаем путь к файлу (для кроссплатформенности, например)
-# path = os.path.join('./static','image0008.png')
-# читаем файл и енкодируем его в строку base64
-with open(path, 'rb') as fh:
-    img_data = fh.read()
-    b64 = base64.b64encode(img_data)
-# создаем json словарь, который
-# отправляется на сервер в виде json строки
-# преобразование делает сама функция отправки запроса post
-# jsondata = {'imagebin':b64.decode('utf-8')}
-res = requests.post('http://localhost:5000/imgfilter')
-if res.ok:
-    print(res.json())
+try:
+    r = requests.get('http://localhost:5000/imgfilter')
+    print(r.status_code)
+    if(r.status_code!=200):
+        exit(1)
+    print(r.text)
+except:
+    exit(1)
